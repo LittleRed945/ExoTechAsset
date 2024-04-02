2ada904b6a1e4a862cb92d66d2e64694d49c1a99
陳謙 <t112598078@ntut.edu.tw>
2024-04-02 18:49:54 +0800

    feat: Add SprintBoot framework
    Signed-off-by: 陳謙 <t112598078@ntut.edu.tw>

---
+++ src/main/kotlin/org/exotechasset/exotechasset/ExotechassetApplication.kt
@@ -0,0 +1,11 @@
+package org.exotechasset.exotechasset
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExotechassetApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExotechassetApplication>(*args)
+}
---
+++ src/test/kotlin/org/exotechasset/exotechasset/ExotechassetApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.exotechasset.exotechasset
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExotechassetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
