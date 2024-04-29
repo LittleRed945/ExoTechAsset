be6a36f099b3d2fc2063f78f4f7433d4065c72e0
陳謙 <t112598078@ntut.edu.tw>
2024-04-29 16:29:43 +0800

    feat: Remove codes of Reporter and Exporter/Importer, which haven't been completed yet
    Signed-off-by: 陳謙 <t112598078@ntut.edu.tw>

--- src/main/kotlin/org/exotechasset/exotechasset/Entity/Chart.kt
+++
@@ -1,8 +0,0 @@
-package org.exotechasset.exotechasset.entity
-
-
-
-abstract class Chart(metrics: Metric): Report(metrics) {
-    protected var datas: MutableMap<AssetGetBy, MutableList<Any>> = mutableMapOf<AssetGetBy, MutableList<Any>>()
-
-}
--- src/main/kotlin/org/exotechasset/exotechasset/UseCase/ReportType.kt
+++
@@ -1,7 +0,0 @@
-package org.exotechasset.exotechasset.useCase
-
-enum class ReportType {
-    TABLE,
-    BAR,
-    PIE,
-}
