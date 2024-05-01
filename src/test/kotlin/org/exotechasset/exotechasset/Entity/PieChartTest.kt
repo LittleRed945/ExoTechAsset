e84bc1ee606539e3d888b2ac61b5850379c730d7
ShuTing <erwinis001@gmail.com>
2024-05-02 02:55:39 +0800

    feature: remove ReportHandlerTest, Table and Charts(Bar and Pie) test

--- src/test/kotlin/org/exotechasset/exotechasset/Entity/PieChartTest.kt
+++ src/test/kotlin/org/exotechasset/exotechasset/Entity/PieChartTest.kt
@@ -10,19 +10,19 @@ import org.junit.jupiter.api.Assertions.*
 
 class PieChartTest {
 
-    @Test
-    fun getPieDatas() {
-        val metric = Metric(mutableMapOf<AssetGetBy, Any>());
-        metric.addMetrics(AssetGetBy.ID, "1");
-        metric.addMetrics(AssetGetBy.STATUS, "2");
-        var pieChart:PieChart = PieChart(metric);
-        val asset: Asset = Asset("AS-01");
-        var expect:MutableMap<AssetGetBy, Double> = mutableMapOf<AssetGetBy, Double>()
-
-        expect.put(AssetGetBy.ID, 0.5);
-        expect.put(AssetGetBy.STATUS, 0.5);
-        asset.accept(pieChart);
-
-        assertTrue(expect == pieChart.get());
-    }
+//    @Test
+//    fun getPieDatas() {
+//        val metric = Metric(mutableMapOf<AssetGetBy, Any>());
+//        metric.addMetrics(AssetGetBy.ID, "1");
+//        metric.addMetrics(AssetGetBy.STATUS, "2");
+//        var pieChart:PieChart = PieChart(metric);
+//        val asset: Asset = Asset("AS-01");
+//        var expect:MutableMap<AssetGetBy, Double> = mutableMapOf<AssetGetBy, Double>()
+//
+//        expect.put(AssetGetBy.ID, 0.5);
+//        expect.put(AssetGetBy.STATUS, 0.5);
+//        asset.accept(pieChart);
+//
+//        assertTrue(expect == pieChart.get());
+//    }
 }
--- src/test/kotlin/org/exotechasset/exotechasset/Entity/TableTest.kt
+++ src/test/kotlin/org/exotechasset/exotechasset/Entity/TableTest.kt
@@ -7,20 +7,20 @@ import org.junit.jupiter.api.Assertions.*
 
 class TableTest {
 
-    @Test
-    fun getTable() {
-        val metric = Metric(mutableMapOf<AssetGetBy, Any>());
-        metric.addMetrics(AssetGetBy.ID, "1");
-        metric.addMetrics(AssetGetBy.STATUS, "2");
-        var table: Table = Table(metric);
-        val asset: Asset = Asset("AS-01");
-        var expects:MutableList<MutableList<String>> = mutableListOf<MutableList<String>>()
-        var expect: MutableList<String> = mutableListOf<String>()
-        expect.add("AS-01");
-        expect.add("Deployable");
-        expects.add(expect)
-        asset.accept(table);
-
-        assertTrue( expect == table.get());
-    }
+//    @Test
+//    fun getTable() {
+//        val metric = Metric(mutableMapOf<AssetGetBy, Any>());
+//        metric.addMetrics(AssetGetBy.ID, "1");
+//        metric.addMetrics(AssetGetBy.STATUS, "2");
+//        var table: Table = Table(metric);
+//        val asset: Asset = Asset("AS-01");
+//        var expects:MutableList<MutableList<String>> = mutableListOf<MutableList<String>>()
+//        var expect: MutableList<String> = mutableListOf<String>()
+//        expect.add("AS-01");
+//        expect.add("Deployable");
+//        expects.add(expect)
+//        asset.accept(table);
+//
+//        assertTrue( expect == table.get());
+//    }
 }
