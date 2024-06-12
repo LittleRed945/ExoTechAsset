0f3893ac0f629ba0904df957f602f540751602a9
陳謙 <t112598078@ntut.edu.tw>
2024-06-13 00:37:22 +0800

    Add more unit test
    Signed-off-by: 陳謙 <t112598078@ntut.edu.tw>

---
+++ src/test/kotlin/org/exotechasset/exotechasset/UseCase/AssetHandlerTest.kt
@@ -0,0 +1,102 @@
+import org.exotechasset.exotechasset.entity.Asset
+import org.exotechasset.exotechasset.entity.AssetStatus
+import org.exotechasset.exotechasset.entity.Date
+import org.exotechasset.exotechasset.usecase.AssetHandler
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.time.Instant
+
+internal class AssetHandlerTest {
+    @Test
+    fun testAssetHandler() {
+        val asset1 = Asset("Asset 1")
+        val asset2 = Asset("Asset 2")
+        val asset3 = Asset("Asset 3")
+
+        val assetHandler = AssetHandler()
+
+        assertEquals(0, assetHandler.size())
+
+        assetHandler.addNewAsset(asset1)
+        assetHandler.addNewAsset(asset2)
+        assetHandler.addNewAsset(asset3)
+
+        assertEquals(3, assetHandler.size())
+
+        assertEquals(asset1, assetHandler.getAsset("Asset 1"))
+        assertEquals(asset2, assetHandler.getAsset("Asset 2"))
+        assertEquals(asset3, assetHandler.getAsset("Asset 3"))
+    }
+
+    @Test
+    fun testAddNewAsset() {
+        val asset1 = Asset(id = "asset1")
+        val assetHandler = AssetHandler()
+        assetHandler.addNewAsset(asset1)
+
+        assertEquals(asset1, assetHandler.getAsset("asset1"))
+    }
+
+    @Test
+    fun testModifyAsset() {
+        val asset1 = Asset(id = "asset1")
+        val assetHandler = AssetHandler(listOf(asset1))
+        val modifiedAsset = Asset(id = "asset1", status = AssetStatus.UNDEPLOYABLE)
+        val result = assetHandler.modifyAsset(modifiedAsset)
+
+        assertTrue(result)
+        assertEquals(AssetStatus.UNDEPLOYABLE, assetHandler.getAsset("asset1")?.getStatus())
+    }
+
+    @Test
+    fun testDeleteAssetById() {
+        val asset1 = Asset(id = "asset1")
+        val assetHandler = AssetHandler(listOf(asset1))
+        assetHandler.deleteAsset("asset1")
+
+        assertFalse(assetHandler.getAsset("asset1") != null)
+    }
+
+    @Test
+    fun testDeleteAsset() {
+        val asset1 = Asset(id = "asset1")
+        val assetHandler = AssetHandler(listOf(asset1))
+        assetHandler.deleteAsset(asset1)
+
+        assertFalse(assetHandler.getAsset("asset1") != null)
+    }
+
+    @Test
+    fun testAuditAsset() {
+        val asset1 = Asset(id = "asset1")
+        val assetHandler = AssetHandler(listOf(asset1))
+        val now = Date.ofNow()
+
+        assetHandler.auditAsset("asset1", now)
+
+        assertEquals(now, assetHandler.getAsset("asset1")?.getAuditDate())
+    }
+
+    @Test
+    fun testGetChildren() {
+        val asset1 = Asset(id = "asset1")
+        val assetHandler = AssetHandler()
+        assetHandler.addNewAsset(asset1)
+
+        assertEquals(listOf(asset1), assetHandler.getChildren())
+    }
+
+    @Test
+    fun testClone() {
+        val asset1 = Asset(id = "asset1")
+        val assetHandler = AssetHandler()
+        assetHandler.addNewAsset(asset1)
+
+        val clonedAssetHandler = assetHandler.cloneAssetList()
+        clonedAssetHandler.deleteAsset(id = "asset1")
+
+        assertNotEquals(listOf(asset1), clonedAssetHandler.getChildren())
+        assertEquals(listOf(asset1), assetHandler.getChildren())
+    }
+
+}
--- src/test/kotlin/org/exotechasset/exotechasset/UseCase/FilterHandlerTest.kt
+++ src/test/kotlin/org/exotechasset/exotechasset/UseCase/FilterHandlerTest.kt
@@ -3,43 +3,43 @@ import org.exotechasset.exotechasset.usecase.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class FilterListTest {
+internal class FilterHandlerTest {
     private val assetHandler = AssetHandler(listOf(Asset("AS-01"), Asset("AS-02"), Asset("AS-03")))
 
     //     @Test
-    //     fun testFilterList() {
-    //         val filterList = FilterList(this.assetList)
+    //     fun testFilterHandler() {
+    //         val filterHandler = FilterHandler(this.assetList)
     //         val filter = Filter("Category", "Electronics")
     //
-    //         assertEquals(0, filterList.size())
+    //         assertEquals(0, filterHandler.size())
     //
-    //         filterList.addFilter(filter)
+    //         filterHandler.addFilter(filter)
     //
-    //         assertEquals(1, filterList.size())
-    //         assertEquals(emptyList<Filter>(), filterList.getFilterList())
+    //         assertEquals(1, filterHandler.size())
+    //         assertEquals(emptyList<Filter>(), filterHandler.getFilterList())
     //     }
     @Test
     fun testGetSize() {
-        val filterList = FilterList(this.assetHandler)
+        val filterHandler = FilterHandler(this.assetHandler)
 
-        assertEquals(0, filterList.size())
+        assertEquals(0, filterHandler.size())
     }
 
     @Test
     fun testGetFilterList() {
-        val filterList = FilterList(this.assetHandler)
+        val filterHandler = FilterHandler(this.assetHandler)
 
-        assertEquals(emptyList<Filter>(), filterList.getFilterList())
+        assertEquals(emptyList<Filter>(), filterHandler.getFilterList())
     }
 
     @Test
     fun testAddFilter() {
-        val filterList = FilterList(this.assetHandler)
+        val filterHandler = FilterHandler(this.assetHandler)
         val filter = EqualsToFilter(FilterField.ID, "AS-01")
 
-        filterList.addFilter(filter)
+        filterHandler.addFilter(filter)
 
-        assertEquals(listOf(filter), filterList.getFilterList())
+        assertEquals(listOf(filter), filterHandler.getFilterList())
     }
 
     @Test
@@ -51,12 +51,12 @@ internal class FilterListTest {
                         Asset("AS-03", auditDate = Date(1969632000))
                 )
 
-        val filterList = FilterList(AssetHandler(assetList2))
+        val filterHandler = FilterHandler(AssetHandler(assetList2))
 
-        filterList.addFilter(GreaterThanFilter(FilterField.AUDIT_DATE, 1714402763.toLong()))
-        filterList.addFilter(LessThanFilter(FilterField.AUDIT_DATE, 1969631000.toLong()))
+        filterHandler.addFilter(GreaterThanFilter(FilterField.AUDIT_DATE, 1714402763.toLong()))
+        filterHandler.addFilter(LessThanFilter(FilterField.AUDIT_DATE, 1969631000.toLong()))
 
-        assertEquals(1, filterList.filterAsset().size())
-        assertEquals(assetList2[1].getId(), filterList.filterAsset().getChildren()[0].getId())
+        assertEquals(1, filterHandler.filterAsset().size())
+        assertEquals(assetList2[1].getId(), filterHandler.filterAsset().getChildren()[0].getId())
     }
 }
